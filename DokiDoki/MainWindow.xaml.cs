b57df96f767873602c5d5ec605418602324446a6
MatthiasHahn <hahn669m@htl-ottakring.ac.at>
2018-02-26 08:40:28 +0100

    Some nice shiez

--- DokiDoki/MainWindow.xaml.cs
+++ DokiDoki/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace DokiDoki
 {
@@ -28,6 +29,29 @@ namespace DokiDoki
         private void wnd_main_MouseDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
+        }
+
+        private void img_next_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Rooms rm = new Rooms();
+            rm.Show();
+            Close();
+        }
+
+        private void img_next_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation(0.4, 0.6, TimeSpan.FromSeconds(0.2));
+            DoubleAnimation anim2 = new DoubleAnimation(0.8, 1, TimeSpan.FromSeconds(0.2));
+            img_next.BeginAnimation(OpacityProperty, anim);
+            lbl_deko_next.BeginAnimation(OpacityProperty, anim2);
+        }
+
+        private void img_next_MouseLeave(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation(0.6, 0.4, TimeSpan.FromSeconds(0.2));
+            DoubleAnimation anim2 = new DoubleAnimation(1, 0.8, TimeSpan.FromSeconds(0.2));
+            img_next.BeginAnimation(OpacityProperty, anim);
+            lbl_deko_next.BeginAnimation(OpacityProperty, anim2);
         }
     }
 }
