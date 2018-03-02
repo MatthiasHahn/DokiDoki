307d8d68e65935922d1a3088f0256e1ae7de8e34
DjoleBoi <jovanovic557d@htl-ottakring.ac.at>
2018-03-03 00:44:47 +0100

    Email Design Finished

--- DokiDoki/Email_Verify.xaml.cs
+++ DokiDoki/Email_Verify.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace DokiDoki
 {
@@ -24,5 +25,55 @@ namespace DokiDoki
             InitializeComponent();
             this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
-    }
+
+        private void wnd_email_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+
+        }
+        private void img_next_em_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation(0.4, 0.6, TimeSpan.FromSeconds(0.2));
+            DoubleAnimation anim2 = new DoubleAnimation(0.8, 1, TimeSpan.FromSeconds(0.2));
+            img_next_em.BeginAnimation(OpacityProperty, anim);
+            lbl_deko_next_em.BeginAnimation(OpacityProperty, anim2);
+        }
+
+        private void img_next_em_MouseLeave(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation(0.6, 0.4, TimeSpan.FromSeconds(0.2));
+            DoubleAnimation anim2 = new DoubleAnimation(1, 0.8, TimeSpan.FromSeconds(0.2));
+            img_next_em.BeginAnimation(OpacityProperty, anim);
+            lbl_deko_next_em.BeginAnimation(OpacityProperty, anim2);
+        }
+
+        private void img_back_em_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation(0.4, 0.6, TimeSpan.FromSeconds(0.2));
+            DoubleAnimation anim2 = new DoubleAnimation(0.8, 1, TimeSpan.FromSeconds(0.2));
+            img_back_em.BeginAnimation(OpacityProperty, anim);
+            lbl_deko_back_em.BeginAnimation(OpacityProperty, anim2);
+        }
+
+        private void img_back_em_MouseLeave(object sender, MouseEventArgs e)
+        {
+            DoubleAnimation anim = new DoubleAnimation(0.6, 0.4, TimeSpan.FromSeconds(0.2));
+            DoubleAnimation anim2 = new DoubleAnimation(1, 0.8, TimeSpan.FromSeconds(0.2));
+            img_back_em.BeginAnimation(OpacityProperty, anim);
+            lbl_deko_back_em.BeginAnimation(OpacityProperty, anim2);
+        }
+        private void img_back_em_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Register rg = new Register();
+            rg.Show();
+            Close();
+        }
+
+        private void img_next_em_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+            Close();
+        }
+        }
 }
